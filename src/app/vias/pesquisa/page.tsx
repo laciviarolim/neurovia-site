bfb076982813f2ec491c37ee87776adae5ef78d1
laciviarolim <laciviajrs35@gmail.com>
2025-04-22 11:34:28 -0300

    Update page.tsx

--- src/app/vias/pesquisa/page.tsx
+++ src/app/vias/pesquisa/page.tsx
@@ -1,13 +1,3 @@
 'use client';
-import React from 'react';
-
-const Page = () => {
-  return (
-    <main className="min-h-screen bg-black text-white p-10">
-      <h1 className="text-4xl font-bold text-cyan-400 mb-4">Via dedicada</h1>
-      <p>Esta é uma página dedicada a esta trilha da Neurovia. Adicione aqui os projetos, conteúdos e informações relevantes para esta área.</p>
-    </main>
-  );
-};
-
-export default Page;
+import { Pesquisa } from '@/app/vias-content-pages';
+export default Pesquisa;
